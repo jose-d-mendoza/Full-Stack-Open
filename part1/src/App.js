50ad90a85acca1a6924a174c95674410dfdec03d
jose-d-mendoza <josedmendoza1024@gmail.com>
2023-03-07 19:21:24 -0800

    Component section

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+// full stack open
 
-function App() {
+/* render dynamic content */
+const App = () => {
+  const now = new Date()
+  const a = 10
+  const b = 20
+  console.log(now, a + b)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>Hello world, it is {now.toString()}</p>
+      <p>
+        {a} plus {b} is {a + b}
+      </p>
     </div>
-  );
+  )
 }
 
-export default App;
+/* console output
+const App = () => {
+  console.log("Hello from component")
+  return (
+    <div>
+      <p>Hello World</p>
+    </div>
+  )
+}
+*/
+
+/* simple
+const App = () => (
+  <div>
+    <p>Hello World</p>
+  </div>
+)
+*/
+
+export default App
