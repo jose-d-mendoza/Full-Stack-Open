7a62f6f4244f21b9b1fece9a4625b94401b7631c
jose-d-mendoza <josedmendoza1024@gmail.com>
2023-03-08 18:21:10 -0800

    Passing Data to Components section

--- part1/src/App.js
+++ part1/src/App.js
@@ -2,8 +2,54 @@
 
 import React from "react"
 
+/***** Props: Passing Data to Components Section *****/
+/* Hello with 2 props */
+const Hello = (props) => {
+  console.log(props)
+  return (
+    <div>
+      <p>
+        Hello {props.name}, you are {props.age} years old
+      </p>
+    </div>
+  )
+}
+
+const App = () => {
+  const name = "Jose"
+  const age = 26
+
+  return (
+    <div>
+      <h1>Greetings</h1>
+      <Hello name="Gloria" age={10 + 16}/>
+      <Hello name={name} age={age}/>
+    </div>
+  )
+}
+
+/* passing hardcoded values
+const Hello = (props) => {
+  return (
+    <div>
+      <p>Hello {props.name}</p>
+    </div>
+  )
+}
+
+const App = () => {
+  return (
+    <div>
+      <h1>Greetings</h1>
+      <Hello name="Jose"/>
+      <Hello name="Gloria"/>
+    </div>
+  )
+}
+*/
+
 /***** Multiple Components Section *****/
-const Hello = () => {
+/* const Hello = () => {
   return (
     <div>
       <p>Hello world</p>
@@ -18,7 +64,7 @@ const App = () => {
       <Hello/>
     </div>
   )
-}
+} */
 
 /***** JSX Section *****/
 /* after compilation (handled by Babel):
