ca6a0e6916e179e0c69c960dca5ac964abd5b768
jose-d-mendoza <josedmendoza1024@gmail.com>
2023-03-12 16:11:04 -0700

    Objects section

--- part1/Code Notes/Variables.js
+++ part1/Code Notes/Variables.js
@@ -60,3 +60,36 @@ console.log(rest) // [3, 4, 5]
 /******* Objects Section *******/
 console.log()
 console.log("******* Objects Section *******")
+
+// object literals
+console.log()
+console.log("--- object literals ---")
+const object1 = {
+    name: "Arto Hellas",
+    age: 35,
+    education: "PhD",
+}
+
+const object2 = {
+    name: "Full Stack web application development",
+    level: "intermediate studies",
+    size: 5,
+}
+
+const object3 = {
+    name: {
+        first: "Dan",
+        last: "Abramov",
+    },
+    grades: [2, 3, 5, 3],
+    department: "Stanford University",
+}
+
+console.log(object1.name) // Arto Hellas
+const fieldName = "age"
+console.log(object1[fieldName]) // 35
+
+object1.address = "Helsinki"
+object1["secret number"] = 12341
+
+
