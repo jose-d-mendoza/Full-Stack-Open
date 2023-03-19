bbc4dc6f99d6ea2a97a95cea3e6025a00744c2ac
jose-d-mendoza <josedmendoza1024@gmail.com>
2023-03-19 11:37:46 -0700

    Complex state section

--- part1/Code Notes/app_notes/src/App.js
+++ part1/Code Notes/app_notes/src/App.js
@@ -1,107 +1,31 @@
 import React from "react"
 import { renderIntoDocument } from "react-dom/test-utils"
 
-/***** Event Handling & 
- * An Event Handler is a Function & 
- * Passing State - To Child Components &
- * Refactoring the Components *****/
+/***** Complext State *****/
 
 import { useState } from "react" // imports useState function
+import { setSelectionRange } from "@testing-library/user-event/dist/utils"
+import { click } from "@testing-library/user-event/dist/click"
 
 const App = () => {
-  const [ counter, setCounter ] = useState(0)
-  console.log("Rendering with counter value", counter)
+  const [clicks, setClicks] = useState({
+    left: 0, right: 0
+  })
 
-  const increaseByOne = () => {
-    console.log("Increasing, value before:", counter)
-    setCounter(counter + 1)
-  }
+  const handleLeftClick = () =>
+    setClicks({...clicks, left: clicks.left + 1})
 
-  const decreaseByOne = () => { 
-    console.log("Decreasing, value before:", counter)
-    setCounter(counter - 1)
-  }
-
-  const setToZero = () => {
-    console.log("Resetting to zero, value before:", counter)
-    setCounter(0)
-  }
+  const handleRightClick = () =>
+    setClicks({...clicks, right: clicks.right + 1})
 
   return (
     <div>
-      <Display counter={counter}/>
-      <Button handleClick={increaseByOne} text="Plus"/>
-      <Button handleClick={decreaseByOne} text="Minus"/>
-      <Button handleClick={setToZero} text="Reset"/>
-    </div>
-  )
-}
-
-const Display = ({counter}) => <div>{counter}</div>
-
-const Button =({handleClick, text}) => <button onClick={handleClick}>{text}</button>
-
-export default App
-
-/***** Stateful Component *****
-
-import { useState } from "react" // imports useState function
-
-const App = () => {
-  const [ counter, setCounter ] = useState(0)
-
-  setTimeout(
-    () => setCounter(counter + 1),
-    1000
-  )
-
-  console.log("rendering...", counter)
-
-  return (
-    <div>{counter}</div>
-  )
-}
-
-export default App
-*/
-
-/***** Page Re-Rendering *****
-
-const App = (props) => {
-  const {counter} = props
-  return (
-    <div>{counter}</div>
-  )
-}
-
-export default App
-*/
-
-/***** Component State, Event Handlers *****/
-/*
-const Hello = ({name, age}) => {
-  const bornYear = () => new Date().getFullYear() - age
-
-  return (
-    <div>
-      <p>Hello {name}, you are {age} years old</p>
-      <p>So you were probably born in {bornYear()}</p>
-    </div>
-  )
-}
-
-const App = () => {
-  const name = "Jose"
-  const age = 26
-
-  return (
-    <div>
-      <h1>Greetings</h1>
-      <Hello name="Gloria" age={16 + 10}/>
-      <Hello name={name} age={age}/>
+      {clicks.left}
+      <button onClick={handleLeftClick}>Left</button>
+      <button onClick={handleRightClick}>Right</button>
+      {clicks.right}
     </div>
   )
 }
 
 export default App
-*/
