75f8d8b441c39af125f31dbf51fe15f265f14592
jose-d-mendoza <josedmendoza1024@gmail.com>
2023-03-19 11:52:20 -0700

    Handling arrays section

--- part1/Code Notes/app_notes/src/App.js
+++ part1/Code Notes/app_notes/src/App.js
@@ -1,9 +1,37 @@
 import React from "react"
 import { renderIntoDocument } from "react-dom/test-utils"
 
-/***** Complext State *****/
-
 import { useState } from "react" // imports useState function
+
+/***** Handling Arrays *****/
+
+const App = () => {
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+  const [allClicks, setAll] = useState([])
+
+  const handleLeftClick = () => {
+    setAll(allClicks.concat("L"))
+    setLeft(left + 1)
+  }
+  
+  const handleRightClick = () => {
+    setAll(allClicks.concat("R"))
+    setRight(right + 1)
+  }
+
+  return (
+    <div>
+      {left}
+      <button onClick={handleLeftClick}>Left</button>
+      <button onClick={handleRightClick}>Right</button>
+      {right}
+      <p>{allClicks.join(" ")}</p>
+    </div>  
+  )
+}
+
+/***** Complext State *****
 
 const App = () => {
   const [clicks, setClicks] = useState({
@@ -25,5 +53,6 @@ const App = () => {
     </div>
   )
 }
+*/
 
 export default App
