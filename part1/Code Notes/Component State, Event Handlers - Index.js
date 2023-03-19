18633560ca3e16b997dd94354e2c696fb9c800da
jose-d-mendoza <josedmendoza1024@gmail.com>
2023-03-19 11:07:44 -0700

    Moving code notes

---
+++ part1/Code Notes/Component State, Event Handlers - Index.js
@@ -0,0 +1,25 @@
+// full stack open
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+import App from "./App"
+
+/***** Stateful Component *****/
+
+ReactDOM.createRoot(document.getElementById("root")).render(<App/>)
+
+/***** Page Re-Rendering *****
+let counter = 1
+
+const refresh = () => {
+    ReactDOM.createRoot(document.getElementById("root")).render(
+        <App counter={counter}/>
+    )
+}
+//ReactDOM.createRoot(document.getElementById("root")).render(<App/>)
+
+setInterval(() => {
+    refresh()
+    counter += 1
+}, 1000)
+*/
