7d6986cea52c61bc6b8cbaad451a098b14aead09
jose-d-mendoza <josedmendoza1024@gmail.com>
2023-03-20 18:28:06 -0700

    Update of the state is asynchronous section

--- part1/Code Notes/app_notes/src/App.js
+++ part1/Code Notes/app_notes/src/App.js
@@ -3,21 +3,27 @@ import { renderIntoDocument } from "react-dom/test-utils"
 
 import { useState } from "react" // imports useState function
 
-/***** Handling Arrays *****/
+/***** Handling Arrays &
+ * Update of the State is Asynchronous *****/
 
 const App = () => {
   const [left, setLeft] = useState(0)
   const [right, setRight] = useState(0)
   const [allClicks, setAll] = useState([])
+  const [total, setTotal] = useState(0)
 
   const handleLeftClick = () => {
     setAll(allClicks.concat("L"))
-    setLeft(left + 1)
+    const updatedLeft = left + 1
+    setLeft(updatedLeft)
+    setTotal(updatedLeft + right)
   }
   
   const handleRightClick = () => {
     setAll(allClicks.concat("R"))
-    setRight(right + 1)
+    const updatedRight = right + 1
+    setRight(updatedRight)
+    setTotal(left + updatedRight)
   }
 
   return (
@@ -27,6 +33,7 @@ const App = () => {
       <button onClick={handleRightClick}>Right</button>
       {right}
       <p>{allClicks.join(" ")}</p>
+      <p>Total: {total}</p>
     </div>  
   )
 }
