54836e82b0a35bd79c889a3e49cfcbbc64817157
jose-d-mendoza <josedmendoza1024@gmail.com>
2023-03-23 18:38:17 -0700

    Exercise 1.7

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -4,18 +4,36 @@ function App() {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [total, setTotal] = useState(0)
+
+  const incGood = (nGood) => {
+    setGood(nGood)
+    setTotal(nGood + neutral + bad)
+  }
+
+  const incNeutral = (nNeutral) => {
+    setNeutral(nNeutral)
+    setTotal(good + nNeutral + bad)
+  }
+
+  const incBad = (nBad) => {
+    setBad(nBad)
+    setTotal(good + neutral + nBad)
+  }
 
   return (
     <div>
       <h1>Give Feedback</h1>
-      <button onClick={() => setGood(good + 1)}>Good</button>
-      <button onClick={() => setNeutral(neutral + 1)}>Neutral</button>
-      <button onClick={() => setBad(bad + 1)}>Bad</button>
+      <button onClick={() => incGood(good + 1)}>Good</button>
+      <button onClick={() => incNeutral(neutral + 1)}>Neutral</button>
+      <button onClick={() => incBad(bad + 1)}>Bad</button>
       <h1>Statistics</h1>
       <p>
         Good: {good} <br/>
         Neutral: {neutral} <br/>
-        Bad: {bad}
+        Bad: {bad} <br/>
+        Average: {(total - neutral - (bad * 2)) / total} <br/>
+        Positive: {(good / total) * 100}%
       </p>
     </div>
   );
