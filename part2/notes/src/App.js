a650c31f212ff4bdf26487710e3a266fc1be03ed
jose-d-mendoza <josedmendoza1024@gmail.com>
2023-03-27 20:05:09 -0700

    Refactoring modules section

--- part2/notes/src/App.js
+++ part2/notes/src/App.js
@@ -1,21 +1,26 @@
 /****** Renering Collections &
  * Key-Attribute &
  * Map &
- * Anti-Pattern: Array Indexes as Keys ******/
+ * Anti-Pattern: Array Indexes as Keys &
+ * Refactoring Modules ******/
 
-const App = (props) => {
-  const {notes} = props
+import Note from "./components/Note"
 
+/* const Note = ({note}) => {
+  return (
+    <li>{note.content}</li>
+  )
+} */
+
+const App = ({notes}) => {
   return (
     <div>
       <h1>Notes</h1>
       <ul>
         {/*notes.map((note, i) => NOT RECOMMENDED!!!
-          <li key={i}> */
-         notes.map(note =>
-          <li key={note.id}>
-            {note.content}
-          </li>
+          <li key={i}> */}
+        {notes.map(note =>
+          <Note key={note.id} note={note}/>
         )}
       </ul>
     </div>
